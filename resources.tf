ecb8aed694f69bda82260350117b533ceafdce4d
olufolarin <folami25@gmail.com>
2022-08-03 18:15:52 +0100

    some changes to the resources file

--- resources.tf
+++ resources.tf
@@ -1,8 +1,7 @@
 resource "aws_iam_user" "multiple_iam_users" {
-#   count = length(var.user_names)
-#   name = var.user_names[count.index]
-
-# to get every unique value from the list
-    for_each = tolist(var.user_names)
-    user_name = each.value #gives u each value
+  #   count = length(var.user_names)
+  #   name = var.user_names[count.index]
+  # to get every unique value from the list
+  for_each  = toset(var.user_names)
+  name = each.value #gives u each value
 }
