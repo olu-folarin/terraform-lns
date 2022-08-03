9077b3c6e465b5c10d4b79b7525861a92a091450
olufolarin <folami25@gmail.com>
2022-08-03 18:10:41 +0100

    used the for_each function to get each/unique value in the list

--- resources.tf
+++ resources.tf
@@ -1,4 +1,8 @@
 resource "aws_iam_user" "multiple_iam_users" {
-  count = length(var.user_names)
-  name = var.user_names[count.index]
+#   count = length(var.user_names)
+#   name = var.user_names[count.index]
+
+# to get every unique value from the list
+    for_each = tolist(var.user_names)
+    user_name = each.value #gives u each value
 }
